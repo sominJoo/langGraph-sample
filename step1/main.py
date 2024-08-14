215cf1acff581cc4347b24f0dcff67496f4d9d61
sominJoo <js06m13@gmail.com>
2024-08-14 17:33:08 +0900

    init: 데이터 연결

--- step1/main.py
+++ step1/main.py
@@ -1,9 +1,25 @@
+import os
+import shutil
+
+import requests
+
 from db_setting import DBSetting
 from step1.agent.graph_part1 import run_graph
 
 
 def main():
     local_db_file = "travel2.sqlite"
+
+    # DB 파일이 존재하지 않는다면 URL에서 다운로드 진행해 local_db 파일 생성
+    db_url = "https://storage.googleapis.com/benchmarks-artifacts/travel-db/travel2.sqlite"
+    backup_file = "travel2.backup.sqlite"
+    overwrite = False
+    if overwrite or not os.path.exists(local_db_file):
+        response = requests.get(db_url)
+        response.raise_for_status()  # Ensure the request was successful
+        with open(local_db_file, "wb") as f:
+            f.write(response.content)
+        shutil.copy(local_db_file, backup_file)
 
     db = DBSetting()
     pandas_module = db.connect_db(local_db_file)  # pd 값을 받아옴
